307ada1b638adadecf54cf9d073119c3930a3292
HalfGl0ve <santiagojimenezcotrini@gmail.com>
2024-09-30 15:41:14 -0500

    Implementacion inicial del header

--- src/components/molecules/Header.jsx
+++ src/components/molecules/Header.jsx
@@ -1,14 +1,16 @@
+import './Header.css'
+
 export default function Header() {
   return (
     <header>
 
         <nav>
-            <img src="" alt="" />
-            <ul>
-                <li>About</li>
-                <li>Contact Us</li>
-            </ul>
-            <section>
+            <img src="/src/img/logo.jpeg" alt="" height={100} width={100}/>
+            <section id='section1'>
+              <p>Servicios Medicos</p>
+              <p>Medios de contacto</p>
+            </section>
+            <section id='section2'>
                 <button>Login</button>
                 <button>Register</button>
             </section>
