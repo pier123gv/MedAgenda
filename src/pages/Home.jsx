5b7f4bfeeddd0eaf16fc0695d3b43fb67807f3bf
DianaPaque <dianas.paquep@autonoma.edu.co>
2024-10-03 20:09:23 -0500

    Inicio diseño pag principal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../components/molecules/Header'
+import './Home.css'
 
 export function Home() {
   return (
-    <>
-    <Header/>
-    <h1>Home</h1>
-    </>
-
-  )
+    <div className="home">
+      <Header />
+      <h1>Bienvenido a MedAgenda</h1>
+      <img src = "/src/img/imgMedicos.jpg" align="right" alt = "" height={300} width={400} />
+    </div>
+  );
 }
