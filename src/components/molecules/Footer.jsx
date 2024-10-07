279f6ff7fb859c0e013f4c785e5a6391af9b420e
HalfGl0ve <santiagojimenezcotrini@gmail.com>
2024-10-07 14:41:25 -0500

    Estilizacion basica del footer e implementacion del formulario de registro

--- src/components/molecules/Footer.jsx
+++ src/components/molecules/Footer.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
+import './Footer.css';
 
 export default function Footer() {
   return (
+    <div className='Footer'>
     <div className="container">
       <footer className="py-5">
         <div className="row">
           <div className="col-6 col-md-2 mb-3">
-            <h5>Section</h5>
+            <h5>Navegación</h5>
             <ul className="nav flex-column">
               <li className="nav-item mb-2">
                 <a href="#" className="nav-link p-0 text-body-secondary">Home</a>
               </li>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">Features</a>
+                <a href="#" className="nav-link p-0 text-body-secondary">Iniciar sesion</a>
               </li>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">Pricing</a>
+                <a href="#" className="nav-link p-0 text-body-secondary">Registrarse</a>
               </li>
               <li className="nav-item mb-2">
                 <a href="#" className="nav-link p-0 text-body-secondary">FAQs</a>
               </li>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">About</a>
+                <a href="#" className="nav-link p-0 text-body-secondary">Nosotros</a>
               </li>
             </ul>
           </div>
@@ -70,17 +72,17 @@ export default function Footer() {
 
           <div className="col-md-5 offset-md-1 mb-3">
             <form>
-              <h5>Subscribe to our newsletter</h5>
-              <p>Monthly digest of what's new and exciting from us.</p>
+              <h5>Recibe información en tu correo</h5>
               <div className="d-flex flex-column flex-sm-row w-100 gap-2">
                 <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
                 <input id="newsletter1" type="text" className="form-control" placeholder="Email address" />
-                <button className="btn btn-primary" type="button">Subscribe</button>
+                <button className="btn btn-primary" type="button">Aceptar</button>
               </div>
             </form>
           </div>
         </div>
       </footer>
+    </div>
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,14 +2,23 @@ import React, { useState } from 'react';
 import './Login.css';
 import Header from '../components/molecules/Header';
 import Footer from '../components/molecules/Footer';
+import { useNavigate } from 'react-router-dom';
+
+
 
 export function Login() {
   const [userType, setUserType] = useState('patient'); // Controla el tipo de usuario
+  const navigate = useNavigate();
 
+  const goToRegisterPage = () => {
+    navigate('/Register');
+  }
   return (
+    <div>
     <div className="login-page">
       <Header/>
       <h1 className="text-h1">Iniciar Sesión</h1>
+      <h6 onClick={goToRegisterPage}>¿No tienes una cuenta?, haz click aqui para crearla</h6>
       <div className="user-type-buttons">
         {/* Botones para elegir el tipo de usuario */}
         <button onClick={() => setUserType('patient')}>Paciente</button>
@@ -18,7 +27,9 @@ export function Login() {
       
       {/* Renderizado condicional del formulario según el tipo de usuario */}
       {userType === 'patient' ? <PatientLoginForm /> : <DoctorLoginForm />}
-      <Footer/>
+      
+    </div>
+    <Footer/>
     </div>
   );
 }
