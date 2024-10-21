48c779d38bcdff5b9125ac3f9e1470fb874dd291
DianaPaque <dianas.paquep@autonoma.edu.co>
2024-10-20 22:54:19 -0500

    Servicios Médicos como tarjetas
    Página para el botón respectivo en el home y las tarjetas tienen traslación

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,22 @@ import Header from '../components/molecules/Header';
 import Footer from '../components/molecules/Footer';
 import './Home.css';
 import Carousel from '../components/organisms/Carousel';
+import { useNavigate } from 'react-router-dom';
 
 export function Home() {
+  const navigate = useNavigate();
+
+  const goToMedicalServicesPage = () => {
+    navigate('/medicalServices');
+};
+
   return (
     <div className="home">
       <Header />
       <div className="recuadro">
         <div className="text-content">
           <h1>Bienvenido a MedAgenda</h1>
-          <button>Servicios médicos que puedes encontrar</button>
+          <button onClick={goToMedicalServicesPage}>Servicios médicos que puedes encontrar</button>
           <button>Novedades y Medios disponibles</button>
         </div>
         <div className="image-content">
--- src/pages/MedicalServices.jsx
+++ src/pages/MedicalServices.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/molecules/Header';
-import './MedicalServices.css'; // Importamos los estilos CSS
+import './MedicalServices.css';
+import Footer from '../components/molecules/Footer';
 
 export function MedicalServices() {
     return (
@@ -10,7 +11,45 @@ export function MedicalServices() {
         <center>
             <h1 className="form-title">Servicios Médicos</h1>
         </center>
-        
+        <div className="medical-services-content">
+            <div className="cards-section">
+            <div className="service-card">
+                <h2>Gestión de citas</h2>
+                <p>Reserva, modificación y cancelación de citas médicas en línea</p>
+            </div>
+            <div className="service-card">
+                <h2>Consultas médicas</h2>
+                <p>Información sobre consultas presenciales o virtuales (telemedicina)</p>
+            </div>
+            <div className="service-card">
+                <h2>Historial médico</h2>
+                <p>Acceso seguro al historial clínico de los pacientes</p>
+            </div>
+            <div className="service-card">
+                <h2>Recetas electrónicas</h2>
+                <p>Generación y envío de recetas electrónicas a los pacientes o farmacias</p>
+            </div>
+            <div className="service-card">
+                <h2>Recordatorios de citas y tratamientos</h2>
+                <p>Notificaciones automáticas sobre próximas consultas o recordatorios de tratamientos</p>
+            </div>
+            <div className="service-card">
+                <h2>Resultados de exámenes</h2>
+                <p>Visualización de resultados de laboratorio y otros estudios médicos</p>
+            </div>
+            <div className="service-card">
+                <h2>Atención especializada</h2>
+                <p>Listado de especialidades médicas disponibles en la clínica</p>
+            </div>
+            <div className="service-card">
+                <h2>Comunicación Integral</h2>
+                <p>Canales, como correos electrónicos y mensajes de texto para intercambio de información</p>
+            </div>
+            </div>
+        </div>
+        </div>
+        <div>
+            <Footer/>
         </div>
     </div>
     );
