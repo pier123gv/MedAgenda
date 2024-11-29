a0c6ccf80b2e4b317cf2519dd4e8570e4bd609b7
pier123gv <daniel.giraldov@autonoma.edu.co>
2024-11-29 12:03:35 -0500

    cambio footer

--- FrontEnd/src/components/molecules/Footer.jsx
+++ FrontEnd/src/components/molecules/Footer.jsx
@@ -1,68 +1,30 @@
 import React from 'react';
 import './Footer.css';
 
-export default function Footer() {
+const Footer = () => {
   return (
-    <div className='Footer'>
-    <div className="container">
-      <footer className="py-5">
-        <div className="row">
-          <div className="col-6 col-md-2 mb-3">
-            <h5>Navegación</h5>
-            <ul className="nav flex-column">
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">Home</a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">Iniciar sesion</a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">Registrarse</a>
-              </li>
-            </ul>
-          </div>
+    <>
+      <div className="b-example-divider"></div> {/* Divider antes del footer */}
+      <div className="container">
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+          <p className="col-md-4 mb-0 text-body-secondary">&copy; 2024 Company, Inc</p>
 
-          <div className="col-6 col-md-2 mb-3">
-            <h5>Contenidos de Bienestar</h5>
-            <ul className="nav flex-column">
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">Servicios medicos</a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">Directorio medico</a>
-              </li>
-            </ul>
-          </div>
+          <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+            <svg className="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap" /></svg>
+          </a>
 
-          <div className="col-6 col-md-2 mb-3">
-            <h5>Políticas de privacidad y uso</h5>
-            <ul className="nav flex-column">
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">Recopilación de información</a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">Protección de datos</a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">Uso de la información</a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="col-md-5 offset-md-1 mb-3">
-            <form>
-              <h5>Recibe información en tu correo</h5>
-              <div className="d-flex flex-column flex-sm-row w-100 gap-2">
-                <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
-                <input id="newsletter1" type="text" className="form-control" placeholder="Email address" />
-                <button className="btn btn-primary" type="button">Aceptar</button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </footer>
-    </div>
-    </div>
+          <ul className="nav col-md-4 justify-content-end">
+            <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
+            <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Features</a></li>
+            <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Pricing</a></li>
+            <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">FAQs</a></li>
+            <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">About</a></li>
+          </ul>
+        </footer>
+      </div>
+    </>
   );
-}
+};
+
+export default Footer;
 
